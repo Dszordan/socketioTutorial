346535e4cffb68de1fa0d0ba6004667faf37e253
Jordan Burnett <jordankburnett@gmail.com>
2015-07-08 22:34:26 -0400

    Added user names and instead of using socket id as identifier, used IP.

--- index.js
+++ index.js
@@ -6,13 +6,32 @@ app.get('/', function(req,res){
 	res.sendFile(__dirname + '/index.html');
 });
 
+var currentUsers = [];
 io.on('connection', function(socket){
-	console.log('a user connected');
+	console.log('a user connected ' + socket.request.headers.host);
+	var currentUser = {ipAddress: socket.request.headers.host, username:'Anonymous'};
+	currentUsers.push(currentUser);
+	io.emit('user list updated', currentUsers);
+
 	socket.on('disconnect', function(){
-		console.log('user disconnected');
+		console.log('user disconnected ' + currentUser.username);
+		io.emit('user list updated', currentUsers);
+		for (var i = 0; i < currentUsers.length; i++) {
+			if (currentUsers[i].ipAddress == socket.request.headers.host) {
+				currentUsers.splice(i,1);
+			};
+		};
 	});
+
+	socket.on('set username', function(username){
+		console.log('username changes : ' + username);
+		currentUser.username = username;
+		io.emit('user list updated', currentUsers);
+	})
+
 	socket.on('chat message', function(msg){
-		io.emit('chat message', msg);
+		var message = {username:currentUser.username, text:msg}
+		io.emit('chat message', message);
 	});
 });
 
